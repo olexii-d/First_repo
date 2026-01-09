c604cab31708e350ac4499dfcadc54e59574bd2a
olexii-d <tokyojapanjapan@gmail.com>
2026-01-10 00:15:57 +0900

    alice

--- main2.py
+++ main2.py
@@ -7,3 +7,6 @@ print(is_adult)
 nums = [3, 1, 4, 1, 5, 9, 2]
 nums.sort()
 print(nums)  # Виведе [1, 1, 2, 3, 4, 5, 9]
+my_dict["age"] = 26  # Змінює вік на 26
+my_dict["email"] = "alice@example.com"  # Додає нову пару ключ-значення
+print(my_dict)
