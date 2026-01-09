eb5200b6443504f17058490f32a37bb7c2f09ec7
olexii-d <tokyojapanjapan@gmail.com>
2026-01-10 00:08:41 +0900

    new3

--- main2.py
+++ main2.py
@@ -4,3 +4,6 @@ print("Commit 3test")
 age = int(input("How old are you? "))
 is_adult = age >= 18
 print(is_adult)
+nums = [3, 1, 4, 1, 5, 9, 2]
+nums.sort()
+print(nums)  # Виведе [1, 1, 2, 3, 4, 5, 9]
